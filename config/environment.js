61c583ed7c18ad73b31de4676390cb165b094dfd
Carlos Salazar <Carlos@desktop-kknqm9r.cc01.local>
2017-05-17 11:29:56 -0500

    fixed some spacing on the environtment file

--- config/environment.js
+++ config/environment.js
@@ -1,57 +1,57 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-    var ENV = {
-        modulePrefix: 'commently',
-        environment: environment,
-        rootURL: '/',
-        locationType: 'auto',
-        EmberENV: {
-            FEATURES: {
-                // Here you can enable experimental features on an ember canary build
-                // e.g. 'with-controller': true
-            },
-            EXTEND_PROTOTYPES: {
-                // Prevent Ember Data from overriding Date.parse.
-                Date: false
-            }
-        },
-        fastboot: {
-            hostWhitelist: [/\.herokuapp.com$/, /^localhost:\d+$/]
-        },
-        DS: {
-            namespace: 'api',
-            host: 'https://commently-api-stage.herokuapp.com'
-        },
-        APP: {
-            // Here you can pass flags/options to your application instance
-            // when it is created
-        }
-    };
-
-    if (environment === 'development') {
-        // ENV.DS.host = 'http://localhost:4000';
-        // ENV.APP.LOG_RESOLVER = true;
-        // ENV.APP.LOG_ACTIVE_GENERATION = true;
-        // ENV.APP.LOG_TRANSITIONS = true;
-        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-        // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  let ENV = {
+    modulePrefix: 'commently',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+    fastboot: {
+      hostWhitelist: [/\.herokuapp.com$/, /^localhost:\d+$/]
+    },
+    DS: {
+      namespace: 'api',
+      host: 'https://commently-api-stage.herokuapp.com'
+    },
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
     }
+  };
 
-    if (environment === 'test') {
-        // Testem prefers this...
-        ENV.locationType = 'none';
+  if (environment === 'development') {
+    // ENV.DS.host = 'http://localhost:4000';
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
 
-        // keep test console output quieter
-        ENV.APP.LOG_ACTIVE_GENERATION = false;
-        ENV.APP.LOG_VIEW_LOOKUPS = false;
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
 
-        ENV.APP.rootElement = '#ember-testing';
-    }
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    if (environment === 'production') {
-        ENV.DS.host = 'https://commently-api.herokuapp.com';
-    }
+    ENV.APP.rootElement = '#ember-testing';
+  }
 
-    return ENV;
+  if (environment === 'production') {
+    ENV.DS.host = 'https://commently-api.herokuapp.com';
+  }
+
+  return ENV;
 };
