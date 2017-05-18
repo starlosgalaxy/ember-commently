e72cf63a9d3495351cd7731d13c78b5b8ebe21e4
Carlos Salazar <Carlos@cindy-pc.cc01.local>
2017-05-18 10:59:33 -0500

    added shoebox for user agent and fixed jshint in tests

--- app/initializers/geo.js
+++ app/initializers/geo.js
@@ -1,18 +1,20 @@
-export function initialize(app) {
+export function initialize(/* app */) {
   // application.inject('route', 'foo', 'service:foo');
-  let geo = navigator.geolocation;
-  app.deferReadiness();
-  geo.getCurrentPosition((pos) => {
-    let pt = pos.coords;
-    let loc = {
-      lat: pt.latitude,
-      lng: pt.longitude
-    };
-    app.register('data:location', loc, {
-      instantiate: false
-    });
-    app.advanceReadiness();
-  });
+  // if (typeof FastBoot === 'undefined') {
+  //   let geo = navigator.geolocation;
+  //   app.deferReadiness();
+  //   geo.getCurrentPosition((pos) => {
+  //     let pt = pos.coords;
+  //     let loc = {
+  //       lat: pt.latitude,
+  //       lng: pt.longitude
+  //     };
+  //     app.register('data:location', loc, {
+  //       instantiate: false
+  //     });
+  //     app.advanceReadiness();
+  //   });
+  // }
 }
 
 export default {
---
+++ app/instance-initializers/request-info.js
@@ -0,0 +1,37 @@
+export function initialize(appInstance) {
+  // appInstance.inject('route', 'foo', 'service:foo');
+
+  let fbService = appInstance.lookup('service:fastboot');
+  let shoebox = fbService.get('shoebox');
+  let shoeboxStore = shoebox.retrieve('request-data');
+  let ua = null;
+  // if rendering on fastboot server make the request
+  if (fbService.get('isFastBoot')) {
+    let headers = fbService.get('request.headers');
+    ua = headers.get('user-agent');
+    console.log('USER AGENT:', ua);
+    appInstance.register('data:request', {
+      userAgent: ua
+    }, {
+      instantiate: false
+    });
+    if (!shoeboxStore) {
+      shoeboxStore = {};
+      shoebox.put('request-data', shoeboxStore);
+    }
+    shoeboxStore.userAgent = ua;
+  } else if (shoeboxStore) {
+      // rendering on the client having been served w/ fastboot
+    ua = shoeboxStore.userAgent;
+    appInstance.register('data:request', {
+      userAgent: ua
+    }, {
+      instantiate: false
+    });
+  } // else do nothing. No ua possible w/ static hosting
+}
+
+export default {
+  name: 'request-info',
+  initialize
+};
