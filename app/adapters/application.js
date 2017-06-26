20bfd5cd9eeeb27cb27b35664cb75bb254e7928b
Carlos Salazar <Carlos@6cb2g72donna-lt.cc01.local>
2017-06-26 09:20:55 -0500

    added url for query record so i could post since changing the data to have an id of current messed things up

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -8,5 +8,38 @@ const { host, namespace } = config.DS;
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:oauth2',
   host,
-  namespace
+  namespace,
+
+  urlForCreateRecord(modelName, snapshot) {
+    switch (modelName) {
+      case 'comment':
+      case 'comments':
+        let postUrl = this.urlForFindRecord(snapshot.belongsTo('post').id, 'post', snapshot);
+        return `${postUrl}/comments`;
+      default:
+        return this._super(...arguments);
+    }
+  },
+
+  urlForDeleteRecord(id, modelName, snapshot) {
+    switch (modelName) {
+      case 'comment':
+      case 'comments':
+        let postUrl = this.urlForFindRecord(snapshot.belongsTo('post').id, 'post', snapshot);
+        return `${postUrl}/comments/${id}`;
+      default:
+        return this._super(...arguments);
+    }
+  },
+
+  urlForQueryRecord({ slug }, modelName) {
+    let baseURL = this.buildURL();
+    switch (modelName) {
+      case 'user':
+      case 'users':
+        return `${baseURL}/users/current`;
+      default:
+        return this._super(...arguments);
+    }
+  }
 });
