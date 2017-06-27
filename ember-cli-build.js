4e60514d3524ca1fbbc71fc204d4560a70018757
Carlos Salazar <Carlos@Carloss-MBP-2.hsd1.tx.comcast.net>
2017-06-27 16:42:47 -0500

    finished hw 16, odd side effecct now adds comments to every file in the pipeline istead fo the final file

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,29 +1,10 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const Filter = require('broccoli-filter');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
 
-  function MyFilter(inputNode) {
-    Filter.call(this, inputNode);
-  }
-
-  MyFilter.prototype = Object.create(Filter.prototype);
-
-  MyFilter.prototype.processString = function(existingString) {
-    let prepend = `/*
-    * vendor.js
-    * (c) 2016 BATOSI175 all rights served
-    * generated at: ${(new Date()).toISOString()}
-    */`;
-    return prepend + existingString;
-  };
-
-  MyFilter.prototype.extensions = ['js'];
-  MyFilter.prototype.targetExtensions = ['js'];
-
-  return new MyFilter(app.toTree());
+  return app.toTree();
 };
